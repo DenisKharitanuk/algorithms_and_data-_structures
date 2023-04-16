3c96850795a4eb44ef17cc5c089cc8ec15602dc7
Den1sprokrastinator <puteec228@gmail.com>
2023-04-16 23:32:10 +0300

    content queue

---
+++ src/queueHW/Queue.java
@@ -0,0 +1,67 @@
+package queueHW;
+
+public class Queue {
+    private int maxSize;
+    private int[] queArray;
+    private int front;
+    private int rear;
+    private int nItems;
+
+    public Queue(int s) {
+        maxSize = s;
+        queArray = new int[maxSize];
+        front = 0;
+        rear = -1;
+        nItems = 0;
+    }
+
+    protected void insert(int j) {
+        if (rear == maxSize - 1) {
+            rear = -1;
+        }
+        queArray[++rear] = j;
+        nItems++;
+
+    }
+
+    protected int remove() {
+        int temp = queArray[front++];
+        if (front == maxSize) {
+            front = 0;
+        }
+        nItems--;
+        return temp;
+    }
+
+    protected int peakFront() {
+        return queArray[front];
+    }
+
+    protected boolean isEmpty() {
+        return (nItems == 0);
+    }
+
+    protected boolean isFull() {
+        return (nItems == maxSize);
+    }
+
+    protected int size() {
+        return nItems;
+    }
+
+    protected void contentOfQueueArray() {
+        for (int i = front; i == queArray.length-1; i++) {
+            System.out.println(queArray[i]);
+
+        }
+        if (rear>front){
+            for (int i = 0 ; i ==rear;i++){
+                System.out.print(queArray[i]+" ");
+            }
+
+        }
+
+    }
+}
+
+
